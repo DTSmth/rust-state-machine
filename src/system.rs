9c91afb5ea01fdb21b3034cfd68f2a15201600b8
DTSmth <ducktapedave85@gmail.com>
2026-03-04 15:07:37 -0500

    Make system create generic

--- src/system.rs
+++ src/system.rs
@@ -1,46 +1,50 @@
 use std::collections::BTreeMap;
-
-type AccountId = String;
-type BlockNumber = u32;
-type Nonce = u32;
-
-
+use std::ops::AddAssign;
+use num::{One, Zero};
 
 #[derive(Debug)]
-pub struct Pallet {
+pub struct Pallet<AccountId, BlockNumber, Nonce > {
     block_number: BlockNumber,
     nonce: BTreeMap<AccountId, Nonce>,
 }
 
-impl Pallet {
+
+impl<AccountId, BlockNumber, Nonce> Pallet<AccountId, BlockNumber, Nonce>
+where
+    AccountId: Ord + Clone,
+    BlockNumber: Zero + One + AddAssign + Copy,
+    Nonce: Zero + One + Copy,
+{
+
     pub fn new() -> Self {
-        Self {
-            block_number: 0,
-            nonce: BTreeMap::new()
-        }
+        Self { block_number: BlockNumber::zero(), nonce: BTreeMap::new() }
     }
+
 
     pub fn block_number(&self) -> BlockNumber {
         self.block_number
     }
 
+
+
     pub fn inc_block_number(&mut self) {
-        self.block_number += 1
+        self.block_number += BlockNumber::one();
     }
+
+
 
     pub fn inc_nonce(&mut self, who: &AccountId) {
-        let nonce = *self.nonce.get(who).unwrap_or(&0);
-        let new_nonce = nonce + 1;
+        let nonce: Nonce = *self.nonce.get(who).unwrap_or(&Nonce::zero());
+        let new_nonce = nonce + Nonce::one();
         self.nonce.insert(who.clone(), new_nonce);
     }
-
 }
 
 #[cfg(test)]
 mod test {
     #[test]
     fn init_system() {
-        let mut system = super::Pallet::new();
+        let mut system = super::Pallet::<String, u32, u32>::new();
         system.inc_block_number();
         system.inc_nonce(&"alice".to_string());
 
