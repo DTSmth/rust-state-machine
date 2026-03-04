e3f29b7a716cfcb8e4c4ba385b0ff57e278331ef
DTSmth <ducktapedave85@gmail.com>
2026-03-04 12:54:16 -0500

    Add type definitions for AccountId Nonce Balance and BlockNumber

--- src/balances.rs
+++ src/balances.rs
@@ -1,8 +1,11 @@
 use std::collections::BTreeMap;
 
+type AccountId = String;
+type Balance = u128;
+
 #[derive(Debug)]
 pub struct Pallet {
-    balances: BTreeMap<String, u128>,
+    balances: BTreeMap<AccountId, Balance>,
 }
 
 impl Pallet {
@@ -11,18 +14,18 @@ impl Pallet {
             balances: BTreeMap::new()
         }
     }
-    pub fn set_balance(&mut self, who: &String, amount: u128) {
+    pub fn set_balance(&mut self, who: &AccountId, amount: Balance) {
         self.balances.insert(who.clone(), amount);
     }
-    pub fn balance(&self, who: &String) -> u128 {
+    pub fn balance(&self, who: &AccountId) -> Balance {
         *self.balances.get(who).unwrap_or(&0)
     }
 
     pub fn transfer(
         &mut self,
-        caller: String,
-        to: String,
-        amount: u128,
+        caller: AccountId,
+        to: AccountId,
+        amount: Balance,
     ) -> Result<(), &'static str> {
         let caller_ballance = self.balance(&caller);
         let to_ballance = self.balance(&to);
--- src/system.rs
+++ src/system.rs
@@ -1,11 +1,15 @@
 use std::collections::BTreeMap;
 
+type AccountId = String;
+type BlockNumber = u32;
+type Nonce = u32;
+
 
 
 #[derive(Debug)]
 pub struct Pallet {
-    block_number: u32,
-    nonce: BTreeMap<String, u32>,
+    block_number: BlockNumber,
+    nonce: BTreeMap<AccountId, Nonce>,
 }
 
 impl Pallet {
@@ -16,7 +20,7 @@ impl Pallet {
         }
     }
 
-    pub fn block_number(&self) -> u32 {
+    pub fn block_number(&self) -> BlockNumber {
         self.block_number
     }
 
@@ -24,7 +28,7 @@ impl Pallet {
         self.block_number += 1
     }
 
-    pub fn inc_nonce(&mut self, who: &String) {
+    pub fn inc_nonce(&mut self, who: &AccountId) {
         let nonce = *self.nonce.get(who).unwrap_or(&0);
         let new_nonce = nonce + 1;
         self.nonce.insert(who.clone(), new_nonce);
